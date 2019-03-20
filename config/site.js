e97632f0102d19b8d160e7e91b692af021266ad3
Tomáš Krmela <openchange@gmail.com>
2019-03-20 09:01:44 +0100

    Update site.js

--- config/site.js
+++ config/site.js
@@ -1,17 +1,17 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  title: 'Gatsby Tutorial Starter', // Navigation and Site Title
-  titleAlt: 'Gatsby Tutorial Starter', // Title for JSONLD
-  description: 'A Gatsby V2 Starter Template Built with a Step By Step Guide',
+  title: 'Secondlife Toulky po SL', // Navigation and Site Title
+  titleAlt: 'Secondlife stokrát a jinak ', // Title for JSONLD
+  description: 'Andel letem světem',
   url: 'https://gatsby-tutorial-starter.netlify.com', // Domain of your site. No trailing slash!
   siteUrl: 'https://gatsby-tutorial-starter.netlify.com', // url + pathPrefix
-  siteLanguage: 'en', // Language Tag on <html> element
+  siteLanguage: 'cs', // Language Tag on <html> element
   logo: 'static/logo/logo.png', // Used for SEO
   banner: 'static/logo/banner.png',
   // JSONLD / Manifest
   favicon: 'static/logo/favicon.png', // Used for manifest favicon generation
-  shortName: 'GatsbyTut', // shortname for manifest. MUST be shorter than 12 characters
-  author: 'Justin', // Author for schemaORGJSONLD
+  shortName: 'andeltut', // shortname for manifest. MUST be shorter than 12 characters
+  author: 'thomask', // Author for schemaORGJSONLD
   themeColor: '#3e7bf2',
   backgroundColor: '#d3e0ff',
   twitter: '@justinformentin', // Twitter Username
